a17cdb6c9d521d13910b820fe09a009cf647a704
Ryan Knuese <knuese@wisc.edu>
2022-01-15 14:56:07 -0600

    add bases to field

--- src/client/components/game/play/Field.tsx
+++ src/client/components/game/play/Field.tsx
@@ -1,10 +1,34 @@
 import { FC } from 'react'
+import classnames from 'classnames'
+import ReactTooltip from 'react-tooltip'
 import field from '../../../assets/field.png'
 
 export const Field: FC = () => {
+  const tooltips = {
+    first: 'Tush',
+    second: 'Bo Bo',
+    third: 'Cupcake'
+  }
+
   return (
     <div className="field-container">
+      <ReactTooltip effect="solid" />
       <img id="field-img" src={field} alt="field" />
+      <span
+        className={classnames('base', { occupied: false })}
+        id="first-base"
+        data-tip={tooltips.first}
+      />
+      <span
+        className={classnames('base', { occupied: true })}
+        id="second-base"
+        data-tip={tooltips.second}
+      />
+      <span
+        className={classnames('base', { occupied: false })}
+        id="third-base"
+        data-tip={tooltips.third}
+      />
       <span className="fielder" id="p">
         Gary
       </span>
